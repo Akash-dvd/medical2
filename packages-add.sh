b6dc209d082a7f0169d0592d7de52acdb0bdd859
akash <akash.d.dwivedi@gmail.com>
2015-04-08 16:21:58 +0530

    package waala v2

--- packages-add.sh
+++ packages-add.sh
@@ -1,28 +1,26 @@
-# Meteor packages used by this project, one per line.
-#
-# 'meteor add' and 'meteor remove' will edit this file for you,
-# but you can also edit it by hand.
-
-meteor add meteor-platform
-meteor add insecure
 meteor add accounts-base
-meteor add accounts-ui
 meteor add accounts-password
-meteor add iron:router
-meteor add mizzao:bootstrap-3
-meteor add pfafman:font-awesome-4
-meteor add chhib:morris
-meteor add email
+meteor add accounts-ui
 meteor add alanning:roles
-meteor add vsivsi:job-collection
 meteor add aldeed:autoform
+meteor add aldeed:autoform-bs-datepicker
 meteor add aldeed:collection2
 meteor add aldeed:simple-schema
-meteor add mrt:moment
-meteor add styling
-meteor add sacha:spin
-meteor add cfs:standard-packages
 meteor add cfs:gridfs
+meteor add cfs:standard-packages
+meteor add chart:chart
+meteor add chhib:morris
+meteor add d3
+meteor add email
+meteor add imap
+meteor add insecure
+meteor add meteor-platform
+meteor add mizzao:bootstrap-3
+meteor add mrt:moment
 meteor add msavin:mongol
 meteor add npm-bcrypt
-meteor remove autopublish
+meteor add pfafman:font-awesome-4
+meteor add sacha:spin
+meteor add styling
+meteor add userman
+meteor add vsivsi:job-collection
